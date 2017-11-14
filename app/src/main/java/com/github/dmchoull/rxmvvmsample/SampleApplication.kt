3db0f25ed9d85b6b97cc2b422eeae94426eb59c3
David <david@retailcommon.com>
2017-11-13 22:23:45 -0500

    added LeakCanary

--- app/src/main/java/com/github/dmchoull/rxmvvmsample/SampleApplication.kt
+++ app/src/main/java/com/github/dmchoull/rxmvvmsample/SampleApplication.kt
@@ -6,6 +6,7 @@ import com.github.dmchoull.rxmvvmsample.di.storeModule
 import com.github.dmchoull.rxmvvmsample.di.viewModelModule
 import com.github.dmchoull.rxmvvmsample.eventbus.EventBus
 import com.github.salomonbrys.kodein.*
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class SampleApplication : Application(), KodeinAware {
@@ -19,6 +20,13 @@ class SampleApplication : Application(), KodeinAware {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
